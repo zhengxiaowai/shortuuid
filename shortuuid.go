5da7fb2d6e89b7674b42a4603ed342afb2b0bdf7
zhengxiaowai <h1x2y3awalm@gmail.com>
2018-12-08 17:17:43 +0800

    feat: generate short uuid

---
+++ shortuuid.go
@@ -0,0 +1,217 @@
+package shortuuid
+
+import (
+	"fmt"
+	"math"
+	"math/big"
+	"sort"
+	"strings"
+
+	crand "crypto/rand"
+	"github.com/satori/go.uuid"
+)
+
+type ShortUUID struct {
+	alphabet    []string
+	alphaLength int
+}
+
+func (su *ShortUUID) GetAlphabet() string {
+	return strings.Join(su.alphabet, "")
+}
+
+func (su *ShortUUID) SetAlphabet(alphabet string) {
+	alphabetTable := removeDuplicates(splitStr(alphabet))
+	sort.Strings(alphabetTable)
+	su.alphabet = alphabetTable
+	su.alphaLength = len(alphabetTable)
+}
+
+func (su *ShortUUID) GetLength() int {
+	return int(math.Ceil(mathLog(math.Pow(2, 128), float64(su.alphaLength))))
+}
+
+func (su *ShortUUID) Encode(uuid string, padLength int) string {
+	if padLength == 0 {
+		padLength = su.GetLength()
+	}
+	return IntToString(uuidToInt(uuid), su.GetAlphabet(), padLength)
+}
+
+func (su *ShortUUID) Decode(str string) uuid.UUID {
+	originBytes := StringToInt(str, su.GetAlphabet()).Bytes()
+	diff := 16 - len(originBytes)
+
+	var fullBytes []byte
+	for diff > 0 {
+		fullBytes = append(fullBytes, 0)
+		diff--
+	}
+
+	if len(fullBytes) != 0 {
+		fullBytes = append(fullBytes, originBytes...)
+	} else {
+		fullBytes = originBytes
+	}
+
+	u, err := uuid.FromBytes(fullBytes)
+	if err != nil {
+		panic(err)
+	}
+	return u
+}
+
+func (su *ShortUUID) GetUUID(name string, padLength int) (string, error) {
+	u := new(uuid.UUID)
+	name = strings.ToLower(name)
+
+	if name == "" {
+		uuidV4, err := uuid.NewV4()
+		if err != nil {
+			return "", err
+		} else {
+			u = &uuidV4
+		}
+	} else if strings.HasPrefix(name, "http://") || strings.HasPrefix(name, "https://") {
+		uuidV5 := uuid.NewV5(uuid.NamespaceURL, name)
+		u = &uuidV5
+	} else {
+		uuidV5 := uuid.NewV5(uuid.NamespaceDNS, name)
+		u = &uuidV5
+	}
+
+	return su.Encode(u.String(), padLength), nil
+}
+
+func (su *ShortUUID) GetUUIDWithNameSpace(name string) string {
+	uuidStr, _ := su.GetUUID(name, su.GetLength())
+	return uuidStr
+
+}
+
+func (su *ShortUUID) UUID() string {
+	return su.GetUUIDWithNameSpace("")
+}
+
+func (su *ShortUUID) Random(length int) string {
+	if length == 0 {
+		length = su.GetLength()
+	}
+
+	// read random file
+	b := make([]byte, length)
+	if _, err := crand.Read(b); err != nil {
+		panic(err)
+	}
+
+	//hexadecimal
+	hexStr := fmt.Sprintf("%x", b)
+
+	i := newBigInt()
+	i.SetString(hexStr, 16)
+	return IntToString(i, su.GetAlphabet(), length)[:length]
+
+}
+
+func NewShortUUID() *ShortUUID {
+	su := new(ShortUUID)
+	su.SetAlphabet("23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
+	return su
+}
+
+func IntToString(number *big.Int, alphabet string, padding int) string {
+	output := ""
+	alphabetTable := splitStr(alphabet)
+	alphabetLength := big.NewInt(int64(len(alphabet)))
+
+	for number.Cmp(big.NewInt(0)) == 1 {
+		quotient, digit := newBigInt().DivMod(number, alphabetLength, newBigInt())
+		output += alphabetTable[digit.Int64()]
+		number = quotient
+	}
+
+	if padding != 0 {
+		remainder := int(math.Max(float64(padding-len(output)), 0))
+		other := ""
+		for remainder > 0 {
+			other += alphabetTable[0]
+			remainder--
+		}
+		output = output + other
+	}
+
+	return reverseStr(output)
+}
+
+func StringToInt(str string, alphabet string) *big.Int {
+	number := newBigInt()
+	alphabetTable := splitStr(alphabet)
+	alphabetLength := big.NewInt(int64(len(alphabet)))
+
+	for _, char := range splitStr(str) {
+		number = number.Mul(number, alphabetLength)
+		charIndex := big.NewInt(int64(indexOf(char, alphabetTable)))
+		number.Add(number, charIndex)
+	}
+
+	return number
+}
+
+func indexOf(char string, chars []string) int {
+	for i, _ := range chars {
+		if chars[i] == char {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func reverseStr(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func splitStr(s string) []string {
+	var ss []string
+	for _, r := range s {
+		c := string(r)
+		ss = append(ss, c)
+	}
+	return ss
+}
+
+func mathLog(x, base float64) float64 {
+	if base == 0 {
+		base = math.E
+	}
+	r := math.Log2(x) / math.Log2(base)
+	return r
+}
+
+func uuidToInt(u string) *big.Int {
+	i := newBigInt()
+	i.SetString(strings.Replace(u, "-", "", 4), 16)
+	return i
+}
+
+func newBigInt() *big.Int {
+	return new(big.Int)
+}
+
+func removeDuplicates(chars []string) []string {
+	encountered := map[string]bool{}
+	var result []string
+
+	for v := range chars {
+		if encountered[chars[v]] == true {
+		} else {
+			encountered[chars[v]] = true
+			result = append(result, chars[v])
+		}
+	}
+	return result
+}
---
+++ shortuuid_test.go
@@ -0,0 +1,200 @@
+package shortuuid
+
+import (
+	"sort"
+	"strings"
+	"testing"
+
+	"github.com/satori/go.uuid"
+)
+
+func TestShortUUID_UUID(t *testing.T) {
+	su := NewShortUUID()
+
+	u1 := su.UUID()
+	u2 := su.GetUUIDWithNameSpace("http://www.example.com/")
+	u3 := su.GetUUIDWithNameSpace("HTTP://www.example.com/")
+	u4 := su.GetUUIDWithNameSpace("example.com/")
+	cases := []string{u1, u2, u3, u4}
+
+	for i, c := range cases {
+		if !(20 < len(c) && len(c) < 24) {
+			t.Errorf("test generation fail, index= %d; uuid = %s", i, c)
+		}
+	}
+
+}
+
+func TestShortUUID_Encode(t *testing.T) {
+	su := NewShortUUID()
+
+	caseTable := map[string]string{
+		"3b1f8b40-222c-4a6e-b77e-779d5a94e21c": "CXc85b4rqinB7s5J52TRYb",
+	}
+
+	for i, o := range caseTable {
+		r := su.Encode(i, 0)
+		if r != o {
+			t.Errorf("test encode fail, %s != %s", i, o)
+		}
+	}
+}
+
+func TestShortUUID_Decode(t *testing.T) {
+	su := NewShortUUID()
+
+	caseTable := map[string]string{
+		"CXc85b4rqinB7s5J52TRYb": "3b1f8b40-222c-4a6e-b77e-779d5a94e21c",
+	}
+
+	for i, o := range caseTable {
+		r := su.Decode(i)
+		if r.String() != o {
+			t.Errorf("test encode fail, %s != %s", r.String(), o)
+		}
+	}
+}
+
+func TestShortUUID_Random(t *testing.T) {
+	su := NewShortUUID()
+	for i := 1; i <= 1000; i++ {
+		u := su.Random(0)
+		if len(u) != 22 {
+			t.Errorf("test random fail, length %d != %d", len(u), 22)
+		}
+	}
+
+	for i := 1; i <= 100; i++ {
+		u := su.Random(i)
+		if len(u) != i {
+			t.Errorf("test random fail, length %d != %d", len(u), i)
+		}
+	}
+
+}
+
+func TestShortUUID_SetAlphabet(t *testing.T) {
+	alphabet := "01"
+	su1 := NewShortUUID()
+	su1.SetAlphabet(alphabet)
+
+	if alphabet != su1.GetAlphabet() {
+		t.Errorf("test SetAlphabet fail, %s != %s", alphabet, su1.GetAlphabet())
+	}
+
+	su1.SetAlphabet("01010101010101")
+
+	if alphabet != su1.GetAlphabet() {
+		t.Errorf("test SetAlphabet fail, %s != %s", alphabet, su1.GetAlphabet())
+	}
+
+	su1UUID := su1.UUID()
+	uuidCharArray := removeDuplicates(splitStr(su1UUID))
+	sort.Strings(uuidCharArray)
+	if strings.Join(uuidCharArray, "") != "01" {
+		t.Errorf("test SetAlphabet fail, %s != %s", su1UUID, "01")
+	}
+
+	if !(116 < len(su1.UUID()) && len(su1.UUID()) < 140) {
+		t.Errorf("test SetAlphabetfail, 116 < (%d) < 140", len(su1.UUID()))
+	}
+
+	su2 := NewShortUUID()
+	su2UUID := su2.UUID()
+	if !(20 < len(su2.UUID()) && len(su2UUID) < 24) {
+		t.Errorf("test SetAlphabet fail, 20 < (%d) < 24", len(su2UUID))
+	}
+
+	for i := 1; i <= 100; i++ {
+		u, err := uuid.NewV4()
+		if err != nil {
+			t.Errorf("test SetAlphabet create uuid v4 fail, error = %s", err)
+		}
+
+		uuidStr := u.String()
+		encodeUUID := su1.Encode(uuidStr, 0)
+		decodeUUID := su1.Decode(encodeUUID).String()
+		if uuidStr != decodeUUID {
+			t.Errorf("test SetAlphabet fail, %s %s %s", uuidStr, encodeUUID, decodeUUID)
+		}
+	}
+
+}
+
+func TestShortUUID_Padding(t *testing.T) {
+	su := NewShortUUID()
+
+	RandomUID, err := uuid.NewV4()
+	if err != nil {
+		t.Fatalf("uuid.NewV4 call err, error = %v", err)
+	}
+
+	zeroBytes := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	smallestUID, err := uuid.FromBytes(zeroBytes)
+	if err != nil {
+		t.Fatalf("uuid.FromBytes call err, error = %v", err)
+	}
+
+	encodedRandom := su.Encode(RandomUID.String(), 0)
+	encodedSmall := su.Encode(smallestUID.String(), 0)
+	if len(encodedRandom) != len(encodedSmall) {
+		t.Errorf("%d != %d", len(encodedRandom), len(encodedSmall))
+	}
+}
+
+func TestShortUUID_Padding_Decode(t *testing.T) {
+	su := NewShortUUID()
+
+	RandomUID, err := uuid.NewV4()
+	if err != nil {
+		t.Fatalf("uuid.NewV4 call err, error = %v", err)
+	}
+
+	zeroBytes := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	smallestUID, err := uuid.FromBytes(zeroBytes)
+	if err != nil {
+		t.Fatalf("uuid.FromBytes call err, error = %v", err)
+	}
+
+	encodedRandom := su.Encode(RandomUID.String(), 0)
+	encodedSmall := su.Encode(smallestUID.String(), 0)
+
+	if su.Decode(encodedSmall).String() != smallestUID.String() {
+		t.Errorf("%s != %s", su.Decode(encodedSmall).String(), smallestUID.String())
+	}
+
+	if su.Decode(encodedRandom).String() != RandomUID.String() {
+		t.Errorf("%s != %s", su.Decode(encodedRandom).String(), RandomUID.String())
+	}
+}
+
+func TestShortUUID_Padding_Consistency(t *testing.T) {
+	su := NewShortUUID()
+	total := 1000
+	uidLengths := make(map[int]int)
+
+	for i := 0; i < total; i++ {
+		RandomUID, err := uuid.NewV4()
+		if err != nil {
+			t.Fatalf("uuid.NewV4 call err, error = %v", err)
+		}
+
+		encodedRandom := su.Encode(RandomUID.String(), 0)
+		uidLengths[len(encodedRandom)] += 1
+		decodeRandom := su.Decode(encodedRandom).String()
+
+		if RandomUID.String() != decodeRandom {
+			t.Errorf("%s != %s", RandomUID.String(), decodeRandom)
+		}
+	}
+
+	if len(uidLengths) != 1 {
+		t.Errorf("%d != %d", len(uidLengths), 1)
+	}
+
+	for _, v := range uidLengths {
+		if v != total {
+			t.Errorf("%d != %d", v, total)
+		}
+	}
+}
